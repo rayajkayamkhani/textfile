cc04fe87d8ea54604349a3ae93638d0e6c52f213
rayaj kayamkhani <rayajkhhan@gamil.com>
2023-02-16 20:36:00 +0530

    change file

--- text.js
+++ text.js
@@ -1,1 +1,131 @@
-console.log("hello");
+// console.log("hello");
+
+
+// let num1 = 22;
+// let num2 = 19;
+
+// let sub = num1-num2;
+// console.log(sub);
+
+// if (sub>19) {
+//     let ab = sub*3;
+//     console.log(ab)
+// } else {
+//     let ab2 = sub*2;
+//     console.log(ab2);
+// }
+
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = 30;
+// let num4 = 10;
+
+// if (num1===num2) {
+//     console.log("yes");
+// }else if(num2===num3){
+//     console.log("yes");
+// }else if (num3==num4){
+//     console.log("yes");
+// }else if(num4===num1){
+//     console.log("yes");
+// }else{
+//     console.log("No");
+// }
+
+
+// function positive_negative(x, y)
+// {
+//   if ((x < 0 && y > 0) || x > 0 && y < 0) 
+//   {
+//     return true;
+//   }
+//   else 
+//   {
+//     return false;
+//   }
+// }
+// console.log(positive_negative(2, 2));
+// console.log(positive_negative(-2, 2));
+// console.log(positive_negative(2, -2));
+// console.log(positive_negative(-2, -2));     
+
+// let num1 = 22;
+// let num2 = 19;
+
+// let sub = num1-num2;
+// console.log(sub);
+
+
+
+// let num =10;
+// let num2 = 10000;
+// switch (num) {
+//     case num:
+//         if (num%2==0) {
+//             console.log("odd")
+//         } else {
+//             console.log("10000")
+//         }
+//         break;
+//     case num:
+//         if (num%2==1) {
+//             console.log("even")
+//         } else {
+//            console.log("10000") 
+//         }
+//         case num:
+//             if (num===100) {
+//                 console.log(num)
+//             } else {
+//                console.log("10000") 
+//             }
+//     default:
+//         console.log(num2)
+//         break;
+// }
+
+// let user = prompt("What is your birth year?");
+// if (user<2004) {
+//     let user2 = prompt("Do you have valid license number?");
+//     let user = prompt("what is your License No ?")
+//     alert("User with license number "+user+" is Ready to drive.");
+// } else {
+//     alert("Age is Not Applicable!!");
+// }
+
+// if (sub>19) {
+//     let ab = sub*3;
+//     console.log(ab)
+// } else {
+//     let ab2 = sub*2;
+//     console.log(ab2);
+// }
+
+
+// let user1 = parseInt(prompt("How many french fries (60rs per piece) do you want to order? "));
+// let user2 = parseInt(prompt("How many burgers (50rs per piece) do you want to order ? "));
+// let user3 = parseInt(prompt("How many plates of chowmin (100rs per plate) do you want to order?"));
+// let user4 = parseInt(prompt("How many plates of Manchurian (80rs per plate) do you want to order?"));
+// let user5 = parseInt(prompt("How many Cokes (50 per piece) do you want to order?"));
+
+
+// let price1 = user1*60;
+// let price2 = user2*50;
+// let price3 = user3*100;
+// let price4 = user4*80;
+// let price5 = user5*50;
+// let total1 = price1+price2+price3+price4+price5;
+// console.log("Your total bill is: "+total1);
+
+// if (user1<=0) {
+//     console.log("your Item Count For French fries is 0")
+// }else if(user2<=0){
+//     console.log("your Item Count For Burgers is 0");
+// }else if(user3<=0){
+//     console.log("your Item Count For Chowmien is 0");
+// }else if(user4<=0){
+//     console.log("your Item Count For Mancurian is 0");
+    
+// }else if(user5<=0) {
+//     console.log("your Item Count For Cokes is 0");
+// }
